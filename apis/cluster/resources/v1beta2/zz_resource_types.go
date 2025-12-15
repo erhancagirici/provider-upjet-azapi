30b862626fcad054ff6f4d3838c6ae8ad166a114
Erhan Cagirici <erhan@upbound.io>
2025-12-15 10:45:01 +0300

    make generate: storage version v1beta1
    Signed-off-by: Erhan Cagirici <erhan@upbound.io>

--- apis/cluster/resources/v1beta2/zz_resource_types.go
+++ apis/cluster/resources/v1beta2/zz_resource_types.go
@@ -717,7 +717,6 @@ type ResourceStatus struct {
 
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
-// +kubebuilder:storageversion
 
 // Resource is the Schema for the Resources API. This resource can manage any Azure Resource Manager resource.
 // +kubebuilder:printcolumn:name="SYNCED",type="string",JSONPath=".status.conditions[?(@.type=='Synced')].status"
